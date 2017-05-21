25c5fc24565e81c76cddd6d105ca508c652f40bb
Matthieu Petit <p.matthieu@me.com>
2017-05-21 15:44:02 +0200

    [+] Add servoblasters to the install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+# to get the motors working
 apt-get install python3 python3-pygame python3-dev python3-rpi.gpio
+
+# servoblaster
+apt-get install git
+git clone https://github.com/richardghirst/PiBits.git
+cd PiBits/ServoBlaster/user
+sudo make install
