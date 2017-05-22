25d9b8dbfec4b0aba1393b6c666f76c38ae4e4e0
Matthieu Petit <p.matthieu@me.com>
2017-05-22 15:10:25 +0200

    [+] Add main test function to motors lib

--- src/motors.py
+++ src/motors.py
@@ -1,7 +1,7 @@
 # CamJam EduKit 3 - Robotics
 # Worksheet 4 – Driving and Turning
 import RPi.GPIO as GPIO 
-import time
+from time import sleep
 # Set the GPIO modes
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -15,8 +15,8 @@ pinMotorBBackwards = 7
 # How many times to turn the pin on and off each second
 Frequency = 20
 # How long the pin stays on each cycle, as a percent (here, it's 30%)
-DutyCycleA = 60
-DutyCycleB = 40
+DutyCycleA = 50
+DutyCycleB = 50
 # Setting the duty cycle to 0 means the motors will not turn
 Stop = 0
 
@@ -40,31 +40,52 @@ pwmMotorBBackwards.start(Stop)
 
 # Turn all motors off
 def StopMotors():
+    print("StopMotors()")
     pwmMotorAForwards.ChangeDutyCycle(Stop)
     pwmMotorABackwards.ChangeDutyCycle(Stop)
     pwmMotorBForwards.ChangeDutyCycle(Stop)
     pwmMotorBBackwards.ChangeDutyCycle(Stop)
 # Turn both motors backwards
 def Backwards(coef = 1):
+    print("Backwards()")
     pwmMotorAForwards.ChangeDutyCycle(DutyCycleA * coef)
     pwmMotorABackwards.ChangeDutyCycle(Stop)
     pwmMotorBForwards.ChangeDutyCycle(DutyCycleB * coef)
     pwmMotorBBackwards.ChangeDutyCycle(Stop)
 # Turn both motors forwards
 def Forwards(coef = 1):
+    print("Forwards()")
     pwmMotorAForwards.ChangeDutyCycle(Stop)
     pwmMotorABackwards.ChangeDutyCycle(DutyCycleA * coef)
     pwmMotorBForwards.ChangeDutyCycle(Stop)
     pwmMotorBBackwards.ChangeDutyCycle(DutyCycleB * coef)
 # Turn left
 def Left(coef = 1):
+    print("Left()")
+    pwmMotorAForwards.ChangeDutyCycle(DutyCycleA * coef)
+    pwmMotorABackwards.ChangeDutyCycle(Stop)
+    pwmMotorBForwards.ChangeDutyCycle(Stop)
+    pwmMotorBBackwards.ChangeDutyCycle(DutyCycleA * coef)
+# Turn Right
+def Right(coef = 1):
+    print("Right")
     pwmMotorAForwards.ChangeDutyCycle(Stop)
     pwmMotorABackwards.ChangeDutyCycle(DutyCycleA * coef)
     pwmMotorBForwards.ChangeDutyCycle(DutyCycleB * coef)
     pwmMotorBBackwards.ChangeDutyCycle(Stop)
-# Turn Right
-def Right(coef = 1):
-    pwmMotorAForwards.ChangeDutyCycle(DutyCycleA * coef)
-    pwmMotorABackwards.ChangeDutyCycle(Stop)
-    pwmMotorBForwards.ChangeDutyCycle(Stop)
-    pwmMotorBBackwards.ChangeDutyCycle(DutyCycleB * coef)
+
+
+def __main__():
+    Forwards()
+    sleep(0.5)
+    Backwards()
+    sleep(0.5)
+    Left()
+    sleep(0.5)
+    Right()
+    sleep(0.5)
+    StopMotors()
+
+if __name__ == "__main__":
+     __main__()
+
